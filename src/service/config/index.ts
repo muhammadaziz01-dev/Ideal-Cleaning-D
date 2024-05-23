b54579bc03cbbc4456d046a0854bfe6d9027e78f
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-05-23 10:40:34 -1200

    refresh token test

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,17 +1,55 @@
 import axios from "axios";
-import {getCookies}from "@cookie"
+import {getCookies, setCookies}from "@cookie"
 
 const http:any = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL  
 })
 
+async function  refreshAccsesToken (){
+     try{
+    const refresh_token = getCookies("refresh-token")
+    // console.log(refresh_token);
+    
+    if(!refresh_token){
+        throw new Error ("Refresh token not found in cookie ") 
+    }else{
+        const response = await axios.post(`https://app.olimjanov.uz/v1/auth/refresh-accesstoken/${refresh_token}`)
+        const {access_token} = response.data;
+        console.log(access_token);
+        setCookies("acses-token", access_token)
+        return access_token;
+    }
+     }catch(error){
+       console.log(error);
+       
+     }
+}
+
  http.interceptors.request.use((config:any)=>{
-    let token = getCookies("acses-token");//localStorage.getItem("token")
+    let token = getCookies("acses-token");
     if(token){
         config.headers.Authorization = ` ${token}`;
     }
     return config;
 })
+
+http.interceptors.response.use((response:any)=>{
+    return response
+}, async (error :any)=>{
+    if(error.response && error.response.status === 401){
+       const access_token =  await refreshAccsesToken();
+    //    console.log(access_token);
+       
+       if(access_token){
+          const originalRequest = error.config
+          originalRequest.headers["Authorization"] = access_token
+       }else{
+         console.error("Access token not found in config file " + error.config)
+         return Promise.reject(error)
+       }
+    }
+})
+
 
 
 export  { http };
